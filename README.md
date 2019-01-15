38bddb2c0b24292aa1fe5dfa7272583cabeebd22
Philip Pannenko <philip.pannenko@gmail.com>
2019-01-14 23:12:08 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+#CICD - Client2
+
+Followed these instructions:
+`https://nodejs.org/en/docs/guides/nodejs-docker-webapp/`
+
+Followed these instructions for running Docker from NPM
+`https://gist.github.com/duluca/d13e501e870215586271b0f9ce1781ce/`
+
+In order to publish the repository
+```
+npm adduser 
+npm publish --access=public
+```
---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import ModifyMessage from '@lablenge/client-utils';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            message: null,
+            configMessage: null
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8080/api/messages")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        message: result.message
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            );
+
+        fetch("http://localhost:8888/cicd/message")
+            .then(res => res.json())
+            .then(result => {
+                    let configMessage = result.propertySources[0].source.message;
+                    this.setState({configMessage});
+                }
+            )
+
+    }
+
+    render() {
+
+        const {error, isLoaded, message, configMessage} = this.state;
+        if (error) {
+            return <div>(cloud config - {configMessage}) Error: {ModifyMessage.addPunctuation(error.message)}</div>;
+        } else if (!isLoaded) {
+            return <div>(cloud config - {configMessage}) Loading...</div>;
+        } else {
+            return <div>(cloud config - {configMessage}) Your message: {ModifyMessage.addPunctuation(message)}</div>
+        }
+    }
+}
+
+export default App;
